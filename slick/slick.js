2e9ca6f2befa66c37a61b98f96e2492e78b44029
Mark Kurt <markkurt@fastmail.fm>
2017-09-29 09:21:42 -0500

    update startup

--- slick/slick.js
+++ slick/slick.js
@@ -16,15 +16,19 @@
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
 ;(function(factory) {
+//https://github.com/kenwheeler/slick/issues/2290
+/*    
     'use strict';
     if (typeof define === 'function' && define.amd) {
         define(['jquery'], factory);
     } else if (typeof exports !== 'undefined') {
         module.exports = factory(require('jquery'));
     } else {
+*/
         factory(jQuery);
+/*
     }
-
+*/
 }(function($) {
     'use strict';
     var Slick = window.Slick || {};
